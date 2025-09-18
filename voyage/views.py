8c6209f3091915973b681892e70604d8bdf40c09
Lemoonchoy <prostonurikivsyo@gmail.com>
2025-09-18 21:29:24 +0500

    error

--- voyage/views.py
+++ voyage/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.core.paginator import Paginator
 from django.contrib.auth import login
 from django.contrib import messages
